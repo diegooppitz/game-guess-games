be90f76b9abde4c660bb4c8e8e53cad6a392fc76
diegooppitz <diego.oppitz@gmail.com>
2022-11-19 13:47:58 -0300

    init use of localStorage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,7 @@
 import { useEffect } from "react";
-import { postLogin } from "../services";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  useEffect(() => {
-    postLogin().then((res) => {
-      console.log(res);
-    });
-  }, []);
-
   return <div className={styles.container}></div>;
 }
